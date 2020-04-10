901ac755eea3e02b91daf0f8e7ef189028c1c483
Josoria <josoriap@gmail.com>
2020-04-10 13:34:33 -0400

    arreglando cventa

--- Vendedores/Vendedores/Vendedores.cpp
+++ Vendedores/Vendedores/Vendedores.cpp
@@ -1,6 +1,6 @@
 // Hacer un programa en C++ que reciba de entrada 10 ventas del mismo número de vendedores y que de como salida:
 // -El promedio de las ventas
-// -La cantidad de ventas mayores o iguales a 50, 000.00.
+// -La cantidad de ventas mayores o iguales a 50,000.00.
 // 
 
 #include <iostream>
@@ -11,27 +11,28 @@ int main()
 {
     int i, cventa ;
     double suma, venta, prom;
-
+    suma = o
+    cventa = 0;
      
     for (i = 0; i <= 9; i++) 
-   
-     {
+        {
         cout << "DIGITE VENTAS DEL VENDEDOR: \n";
         cin >> venta;
         cout << "\n";
         
-        cventa = 0;
-        if (venta >= 50000) {
-		
-		suma = suma + venta; 
-        cventa = cventa + 1;
-		
-		}
-        
-        
-       }
+           if (venta >= 50000) 
+              {
+		       cventa = cventa + 1;
+           }
+              
+           suma = suma + venta;
+           prom = suma / 10
+	   
+    }
             
-        cout << "CANTIDAD VENDIDOS MAYOR O IGUAL A 50 MIL: " << cventa << endl;
+    cout << "EL PROMEDIO DE VENTA ES: " << prom << endl;
+    cout << "CANTIDAD VENDIDOS MAYOR O IGUAL A 50 MIL: " << cventa << endl;
+
 
 
       system("pause");
